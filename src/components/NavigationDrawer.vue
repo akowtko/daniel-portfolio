d8608b003a0c47690c5a0afe43557493a944e03f
Nicole Kowtko <akowtko@hmc.edu>
2019-06-02 14:53:40 -0700

    add navigation drawer and put in sub component, move links into separate file

---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-navigation-drawer
+    dark
+    fixed
+    disable-resize-watcher
+    app
+    v-model="dialogModel"
+  >
+    <v-list>
+      <v-list-tile @click="navigationClick('home', 0)">
+        <v-list-tile-avatar>
+          <v-img :src="headshot" />
+        </v-list-tile-avatar>
+        <v-list-tile-title>
+          {{ name }}
+        </v-list-tile-title>
+      </v-list-tile>
+      <v-divider light/>
+      <v-list-tile @click="navigationClick('about', 0)">
+        <v-list-tile-avatar>
+          <v-icon>contacts</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-title>About</v-list-tile-title>
+      </v-list-tile>
+      <v-list-tile @click="navigationClick('skills', 0)">
+        <v-list-tile-avatar>
+          <v-icon>extension</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-title>Skills</v-list-tile-title>
+      </v-list-tile>
+      <v-list-tile @click="navigationClick('experience', 1)">
+        <v-list-tile-avatar>
+          <v-icon>work_outline</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-title>Experience</v-list-tile-title>
+      </v-list-tile>
+      <v-list-tile @click="navigationClick('publications', 2)">
+        <v-list-tile-avatar>
+          <v-icon>description</v-icon>
+        </v-list-tile-avatar>
+        <v-list-tile-title>Publications</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+
+</template>
+
+<script>
+const { name } = require('../assets/data/about');
+const { headshot } = require('../assets/data/links');
+
+export default {
+  name: 'navigation-drawer',
+  props: {
+    dialogModel: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      name,
+      headshot,
+    };
+  },
+  methods: {
+    navigationClick(refName, tab) {
+      this.$emit('navigation-click', refName, tab);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
